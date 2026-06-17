e77c54c0f2e07786975ce72209fe7aeb88631350
Darion Kellar <credkellar@gmail.com>
2026-06-17 05:17:32 -0700

    Add function to archive Notion entries

---
+++ 28_notion_archive_cleanup.js
@@ -0,0 +1,5 @@
+async function archiveNotionEntry(pageId) {
+    // Moves page to Archive Database[span_8](start_span)[span_8](end_span)
+    console.log(`🗄️ [NOTION] Archiving entry: ${pageId}`);
+}
+module.exports = { archiveNotionEntry };
