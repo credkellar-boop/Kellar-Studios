6b0a6807eb32d032670bc89ece34954490843ab8
Darion Kellar <credkellar@gmail.com>
2026-06-17 05:15:48 -0700

    Implement geometry compliance checker function
    Added a function to check video geometry compliance.

---
+++ 27_geometry_compliance_checker.js
@@ -0,0 +1,7 @@
+const ffmpeg = require('fluent-ffmpeg');
+
+async function checkGeometry(filePath) {
+    // Probes video metadata to ensure no unintended stretching[span_6](start_span)[span_6](end_span)
+    console.log(`📐 [GEOMETRY] Validating screen aspect ratios...`);
+}
+module.exports = { checkGeometry };
